e1ffab609da83dfdd05c20a61869687f18d2e0a0
testautomation-01-2018 <testautomation-01-2018@wp.pl>
2018-01-21 21:00:51 +0100

    i jeszcze raz commit

--- src/test/java/com/jsystems/FirstJUnitTest.java
+++ src/test/java/com/jsystems/FirstJUnitTest.java
@@ -74,7 +74,6 @@ public class FirstJUnitTest extends ConfigJUnit {
         assertThat(list3, IsEmptyCollection.empty());
         assertTrue(list2.containsAll(list2a));
         assertTrue(true == true);
-
     }
 
 
