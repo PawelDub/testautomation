736c4ffd10a18465f9d014b6adbe8e8154b01801
testautomation-01-2018 <automation012018@wp.pl>
2018-01-30 21:09:15 +0100

    Poprawki i pełne uruchomienie

--- src/test/java/com/jsystems/JunitTests/ParametrizedJupiterTest.java
+++ src/test/java/com/jsystems/JunitTests/ParametrizedJupiterTest.java
@@ -1,13 +1,17 @@
 package com.jsystems.JunitTests;
 
-import org.junit.Test;
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,11 +21,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-@RunWith(JUnitPlatform.class)
+//@RunWith(JUnitPlatform.class)
+    @DisplayName("=======Nad klasą główną to są testy sparametryzowane jupiterem ==========")
+    @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ParametrizedJupiterTest extends ConfigJUnit {
 
     @Nested
-    @DisplayName("======= to są testy sparametryzowane jupiterm ==========")
+    @DisplayName("======= to są testy sparametryzowane jupiterem ==========")
     public class JupiterParameterizedTest {
 
 
@@ -50,6 +56,44 @@ public class ParametrizedJupiterTest extends ConfigJUnit {
             assertFalse(listaStringow.equals(tekst));
             assertFalse(listaIntegerow.equals(length));
         }
+
+        @ParameterizedTest
+        @ValueSource(strings = { "Hello", "JUnitlo" })
+        void withValueSource(String word) {
+            assertTrue(word.contains("lo"));
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = { "55", "5" })
+        public void withValueSource2(String word) {
+            assertTrue(word.contains("5"));
+            int a = Integer.parseInt(word);
+            assertTrue(a == 5);
+        }
+
+        @ParameterizedTest
+        @ValueSource(ints = { 1, 3, 5 })
+        void testWithValueSource(int argument) {
+            assertTrue(argument % 2 != 0);
+        }
+
+        @ParameterizedTest
+        @ValueSource(longs = { 2,4,6 })
+        public void withValueLongs(long digit) {
+            assertTrue(digit % 2 == 0 );
+        }
+
+        @ParameterizedTest
+        @CsvFileSource(resources = "/plik.csv")
+        public void withFile(String name, Integer age){
+            System.out.println(name);
+            System.out.println(age);
+            assertTrue(name instanceof String);
+            assertTrue(name.contains(","));
+            assertTrue(age instanceof Integer );
+        }
+
+
     }
 
     @Nested
